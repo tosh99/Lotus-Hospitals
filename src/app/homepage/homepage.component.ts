6c4880144924b5442ba9f0404b8c0ebf3684e54a
Anutosh <anutosh.chaudhuri@gmail.com>
2020-11-12 19:22:26 +0530

    Updated Pages - Finalisation Step 3

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
@@ -10,16 +10,32 @@ export class HomepageComponent implements OnInit {
     slideIndex = 1;
     stopAnimation = false;
     isLeftAwardsDisplayed = true;
+    screenHeight;
+    screenWidth;
+    isDesktop = true;
+
+
+    @HostListener('window:resize', ['$event'])
+    getScreenSize(event?): void {
+        this.screenHeight = window.innerHeight;
+        this.screenWidth = window.innerWidth;
+    }
+
 
     constructor(private title: Title, private meta: Meta) {
         this.title.setTitle('Quality Healthcare at Lotus Hospitals, Hyderabad & Vizag');
         this.meta.updateTag({name: 'description', content: 'We are central to your family\'s comprehensive care. Find Lotus Hospitals near you - Lakdi ka Pul, Kukatpally, LB Nagar & Visakhapatnam.'});
+        this.getScreenSize();
     }
 
     ngOnInit(): void {
         window.scrollTo(0, 0);
         this.showSlides();
-        this.showAwards();
+
+        if (this.screenWidth < 768) {
+            this.isDesktop = false;
+            this.showAwards();
+        }
     }
 
     showAwards(): void {
