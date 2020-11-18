b079c933654719f7061bd05392b98ba2552981e7
Anutosh <anutosh.chaudhuri@gmail.com>
2020-11-18 19:19:02 +0530

    Final Deployment (lotushospitals.in)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Lotus Hospitals';
+    title = 'Lotus Hospitals';
+
+
+    constructor(private router: Router) {
+        this.router.events.subscribe(event => {
+
+            if (event instanceof NavigationEnd) {
+                console.log(event.urlAfterRedirects);
+                gtag('config', 'G-NCNJRGY2WH');
+
+            }
+
+        });
+    }
 }
